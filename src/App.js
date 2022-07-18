41fa5ff29d51e865e5dc39f2a17511a3af32b242
김동호 <ehdrh321@gmail.com>
2022-07-18 23:12:03 +0900

    07/18 23:11 Add : App.js 더미데이터 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 import DiaryEditor from './component/DiaryEditor';
+import DiaryList from './component/DiaryList';
+
+const dummyList = [
+  {
+    id:1,
+    author:"김동호",
+    content:"하이 1",
+    emotion:5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id:2,
+    author:"김동호",
+    content:"하이 2",
+    emotion:2,
+    created_date: new Date().getTime(),
+  },
+  {
+    id:3,
+    author:"김동호",
+    content:"하이 3",
+    emotion:3,
+    created_date: new Date().getTime(),
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <h2>일기장</h2>
       <DiaryEditor/>
+      <DiaryList daiaryList={dummyList}/>
     </div>
   );
 }
