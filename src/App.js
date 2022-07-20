a2693c9aa88838951e4e1dbff08cea03903b6f85
김동호 <ehdrh321@gmail.com>
2022-07-20 22:46:47 +0900

    07/20 22:46 Add : App.js 일기 추가 로직 작성

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import logo from './logo.svg';
+import {useRef, useState} from "react";
+
+/* import logo from './logo.svg';
 import './App.css';
 import DiaryEditor from './component/DiaryEditor';
 import DiaryList from './component/DiaryList';
@@ -25,13 +27,33 @@ const dummyList = [
     emotion:3,
     created_date: new Date().getTime(),
   }
-]
+] */
+
+import { useReducer, useState } from "react";
 
 function App() {
+
+  const [data,setDate] = useState([]);
+
+  const dataId = useRef(0)
+
+  const onCreate = (author,content,emotion) => {
+    const create_date = new Date().getTime();
+    const newItem ={
+      author,
+      content,
+      emotion,
+      create_date,
+      id : dataId.current
+    }
+    dataId.current += 1;
+    setData([ newItem,...data]);
+  };
+
   return (
     <div className="App">
-      <DiaryEditor/>
-      <DiaryList diaryList={dummyList}/>
+      <DiaryEditor onCreate={onCreate}/>
+      <DiaryList diaryList={data}/>
     </div>
   );
 }
