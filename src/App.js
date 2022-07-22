14397e9f33d3e2ccf9a4f02da8876fc840e0846c
김동호 <ehdrh321@gmail.com>
2022-07-22 23:06:21 +0900

    07/22 23:05 Refactor : onDelete > onRemove 이름 변경 및 handleRemove함수화

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ function App() {
     setDate([ newItem,...data]);
   };
 
-  const onDelete = (targetId) => {
+  const onRemove = (targetId) => {
     console.log(`${targetId}가 삭제되었습니다.`)
     const newDiaryList = data.filter((it) => it.id !== targetId);
     setDate(newDiaryList);
@@ -60,7 +60,7 @@ function App() {
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate}/>
-      <DiaryList onDelete={onDelete}diaryList={data}/>
+      <DiaryList onRemove={onRemove}diaryList={data}/>
     </div>
   );
 }
