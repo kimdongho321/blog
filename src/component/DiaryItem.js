15cf0e741f23e455310fb5e0271b7056cb2010a3
김동호 <ehdrh321@gmail.com>
2022-07-22 23:14:50 +0900

    07/22 23:13 Add : DiaryItem.js useState,삼항연산자를 사용한 게시글 수정 기능

--- src/component/DiaryItem.js
+++ src/component/DiaryItem.js
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 const DiaryItem = ({onRemove,author, content, created_date, emotion, id}) => {
+
+    const [isEdit,setIsEdit] = useState(false);
+    const toggleIsEdit = () => setIsEdit(!isEdit);
+
+    const [localContent,setLocalContent] = useState(content);
 
     const handleRemove = () =>{
         if(window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)){
@@ -11,9 +18,31 @@ const DiaryItem = ({onRemove,author, content, created_date, emotion, id}) => {
                 <span>작성자 : {author} | 감정점수 : {emotion}</span><br/>
                 <span className="date">{new Date(created_date).toLocaleString()}</span>
             </div>
-            <div className="content">{content}</div>
-            <button onClick={handleRemove}>삭제하기</button>
-            <button>수정하기</button>
+            <div className="content">{isEdit ? (
+            <>
+                <textarea
+                value={localContent}
+                onChange={(e)=>setLocalContent(e.target.value)}
+                />
+            </>
+            ) : (
+                <>{content}</>
+            )}
+            </div>
+
+            {isEdit ? (
+                <>
+                    <button onClick={handleRemove}>수정 취소</button>
+                    <button onClick={toggleIsEdit}>수정 완료</button>
+                </>
+                ) :(
+                <>
+                    <button onClick={handleRemove}>삭제하기</button>
+                    <button onClick={toggleIsEdit}>수정하기</button>\
+                </>
+            )}
+            
+            
         </div>
     )
 }
