d73afe36f212321e8f483d79f80bd1ec47405584
김동호 <ehdrh321@gmail.com>
2022-07-25 21:52:15 +0900

    07/25 21:51 Add : LifeCycle.js count함수,버튼 추가

--- src/component/LifeCycle.js
+++ src/component/LifeCycle.js
@@ -1,5 +1,24 @@
 import React, { useEffect, useState } from "react";
 
 const LifeCycle = () => {
-    return <div> </div>
+    const [count,setCount] = useState(0);
+    const [text,setText] = useState("");
+
+    useEffect(()=>{
+        console.log("Mount!");
+    },[])
+
+    return (
+    <div>
+        <div>
+            {count}
+            <button onClick={() => setCount(count+1)}>+</button>
+        </div>
+        <div>
+            <input value={text} onChange={(e)=> setText(e.target.value)}></input>
+        </div>
+    </div>
+    )
 }
+
+export default LifeCycle;
