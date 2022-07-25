d7269328c388396e493fce31df060dbb17309d8e
김동호 <ehdrh321@gmail.com>
2022-07-25 21:57:43 +0900

    07/25 21:57 Add : LifeCycle.js 조건문 useEffect

--- src/component/LifeCycle.js
+++ src/component/LifeCycle.js
@@ -14,6 +14,10 @@ const LifeCycle = () => {
 
     useEffect(()=>{
         console.log(`count is update : ${count}`);
+        if(count > 5){
+            alert("count가 5를 넘었습니다. 1로 초기화합니다.");
+            setCount(1);
+        }
     },[count])
 
     useEffect(()=>{
