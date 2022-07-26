8a17cdc7c2eb02b7759f702399aaf6446537d669
김동호 <ehdrh321@gmail.com>
2022-07-26 21:02:19 +0900

    07/26 21:01 Add : App.js map함수를 이용한 데이터 수만큼의 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -40,10 +40,22 @@ function App() {
   const dataId = useRef(0)
 
   const getData = async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/comments").then(
-      (res) => res.json()
-      );
-  }
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime() + 1,
+        id: dataId.current++
+      };
+    });
+    setDate(initData);
+  };
+
   const onCreate = (author,content,emotion) => {
     const create_date = new Date().getTime();
     const newItem ={
